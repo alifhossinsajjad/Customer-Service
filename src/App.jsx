478da2d2fd55771473f035e3711f37869fc98a97
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-25 00:11:31 +0600

    added json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Banner from './Components/Banner/Banner'
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       
-     
+     <Banner/>
       
     </>
   )
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='grid grid-cols-2 gap-5 my-14 '>
+            <div className='flex flex-col text-center justify-center rounded-md p-7 text-white h-64  bg-gradient-to-l bg-[#632EE3] to'>
+                <h2>pendding</h2>
+                <p>0</p>
+            </div>
+            <div className='flex flex-col text-center justify-center rounded-md p-7 text-white h-64  bg-red-500'>
+                <h2>pendding</h2>
+                <p>0</p>
+            </div>
         </div>
     );
 };
