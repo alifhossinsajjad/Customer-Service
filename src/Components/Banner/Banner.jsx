bfc824fb3a001b20c638b3e3d8db4ef3a3ae583d
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-25 06:50:53 +0600

    text style update

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,17 +8,23 @@ const Banner = () => {
         <Counter>
             <div className='grid grid-cols-2 gap-5 my-14 '>
 
-                <div  className=' ' style={{ backgroundImage: `url(${vactor1})`}} >
-             <div className='flex flex-col justify-center bg-gradient-to-r  h-50 from-[#632EE3] to-[#9F62F2] text-white rounded-xl text-center shadow p-6' >
-                        <h2>pendding</h2>
-                        <p>0</p>
+                <div className=' '
+                    style={{
+                        backgroundImage: `url(${vactor1}), url(${vactor2})`,
+                        backgroundPosition: 'left center, right center',
+                        backgroundSize: '50% 100%, 50% 100%',
+                        backgroundRepeat: 'no-repeat, no-repeat'
+                    }} >
+                    <div className='flex flex-col justify-center bg-gradient-to-r  h-50 from-[#632EE3] to-[#9F62F2] text-white rounded-xl text-center shadow p-6' >
+                        <h2 className='text-2xl'>In-Progress</h2>
+                        <p className='text-6xl font-semibold'>0</p>
 
                     </div>
                 </div>
 
                 <div className='flex flex-col justify-center bg-gradient-to-r h-50 from-[#54CF68] to-[#00827A] text-white rounded-xl p-6 text-center shadow' >
-                    <h2>pendding</h2>
-                    <p>0</p>
+                    <h2 className='text-2xl'>Resolved</h2>
+                    <p className='text-6xl font-semibold'>0</p>
                 </div>
             </div>
         </Counter>
