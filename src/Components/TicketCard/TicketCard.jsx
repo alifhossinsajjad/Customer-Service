3348808624655959f130e372c45d61cf87a3215b
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-26 02:51:58 +0600

    title status update

--- src/Components/TicketCard/TicketCard.jsx
+++ src/Components/TicketCard/TicketCard.jsx
@@ -1,15 +1,18 @@
 
 import vactor2 from '../../assets/date.png'
 
-const TicketCard = ({ ticket, handleAddToTask}) => {
-    
+const TicketCard = ({ ticket, handleAddToTask }) => {
+
 
     return (
         <div className='lg:w-full' >
-            <div className='bg-[#ffffff] p-5' onClick={() => {handleAddToTask(ticket)}}>
+            <div className='bg-[#ffffff] p-4' onClick={() => { handleAddToTask(ticket) }}>
                 <div className='flex justify-between mb-2'>
-                    <span className='text-md font-medium' >{ticket.title}</span>
-                    <span className={`flex items-center  rounded-xl mr-2 py-1 px-3 shadow ${ticket.status == "Open" ? 'text-[#0B5E06] font-semibold bg-[#B9F8CF] ' : 'text-[#FEBB0C] bg-yellow-100'}`}>{ticket.status}</span>
+                    <span className='text-md  font-medium' >{ticket.title}</span>
+                    <div className={`flex items-center  rounded-xl gap-2 py-1 px-3 shadow ${ticket.status == "Open" ? ' text-[#0B5E06] font-semibold bg-[#B9F8CF] ' : 'text-[#FEBB0C] bg-yellow-100'}`}>
+                        <p className={`w-4 h-4 rounded-full ${ticket.status == "Open" ? 'bg-[#0B5E06]' :'bg-[#FEBB0C]'}`}></p>
+                        <span >{ticket.status}</span>
+                    </div>
                 </div>
                 <span className='text-gray-500 '>
                     {ticket.description}
@@ -17,7 +20,7 @@ const TicketCard = ({ ticket, handleAddToTask}) => {
 
 
                 <div className='flex justify-between items-center mt-3'>
-                    <div className='flex gap-3'>
+                    <div className='flex items-center gap-3'>
                         <span className='text-gray-500 fontb'>{ticket.id}</span>
                         <span className={`font-semibold   ${ticket.priority == "High Priority" ? 'text-[#F83044]' : ticket.priority == "Low Priority" ? 'text-[#0B5E06]' : "text-[#FEBB0C]"}`}>{ticket.priority}</span>
                     </div>
